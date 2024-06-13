f8dcc11127352859e382545c4f2d6df4365636f4
Erlan Rangel <erlanrangel@gmail.com>
2024-06-13 10:10:56 -0600

    feat: CRUD for RolModel

--- src/main/java/org/cenfotec/mvcpractice/controller/RolController.java
+++ src/main/java/org/cenfotec/mvcpractice/controller/RolController.java
@@ -1,5 +1,6 @@
 package org.cenfotec.mvcpractice.controller;
 
+import org.cenfotec.mvcpractice.dao.PersonRolDAO;
 import org.cenfotec.mvcpractice.dao.RolDAO;
 import org.cenfotec.mvcpractice.model.ConnectionModel;
 import org.cenfotec.mvcpractice.model.RolModel;
@@ -7,15 +8,19 @@ import org.cenfotec.mvcpractice.view.ConsoleView;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RolController {
     private ConsoleView consoleView;
     private RolDAO rolDao;
+    private PersonRolDAO personRolDAO;
 
     public RolController(ConsoleView consoleView) {
         this.consoleView = consoleView;
         Connection connection = ConnectionModel.getConnection();
         this.rolDao = new RolDAO(connection);
+        this.personRolDAO = new PersonRolDAO(connection);
     }
 
     public void addRol(String name, String description) {
@@ -26,6 +31,45 @@ public class RolController {
             consoleView.showMessage("Rol agregado con éxito");
         } catch (SQLException e) {
             consoleView.errorMessage("Error al agregar rol: " + e.getMessage());
+        }
+    }
+
+    public List<RolModel> getAllRoles() {
+        List<RolModel> roles = new ArrayList<>();
+        try {
+            roles = rolDao.findAll();
+        } catch (SQLException e) {
+            consoleView.errorMessage("Error al consultar roles: " + e.getMessage());
+        }
+        return roles;
+    }
+
+    public RolModel getRolById(int id) {
+        RolModel rol = null;
+        try {
+            rol = rolDao.findById(id);
+        } catch (SQLException e) {
+            consoleView.errorMessage("Error al consultar rol: " + e.getMessage());
+        }
+        return rol;
+    }
+
+    public void updateRole(RolModel rol) {
+        try {
+            rolDao.updateRole(rol);
+            consoleView.showMessage("Rol actualizado correctamente.");
+        } catch (SQLException e) {
+            consoleView.errorMessage("Error al actualizar rol: " + e.getMessage());
+        }
+    }
+
+    public void deleteRole(RolModel rol) {
+        try {
+            personRolDAO.deleteRol(rol);
+            rolDao.deleteRole(rol);
+            consoleView.showMessage("Rol eliminado correctamente.");
+        } catch (SQLException e) {
+            consoleView.errorMessage("Error al eliminar rol: " + e.getMessage());
         }
     }
 }
